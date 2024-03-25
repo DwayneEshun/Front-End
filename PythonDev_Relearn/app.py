3e6c890eb94a5df2a8507159222178622d22f77c
DwayneEshun <dwayneeshun@gmail.com>
2024-03-25 06:07:33 +0000

    Commit 1

---
+++ PythonDev_Relearn/app.py
@@ -0,0 +1,68 @@
+name  = 'Jennifer'
+print(name[1:-1])
+# BUILT IIN METHODS FOR STRINGS
+'''name.upper()
+name.lower()
+name.find('J')
+name.replace()
+"red" in name
+name.title()
+'''
+
+#MATH FUNCTIONS
+'''import math
+import math makes it possible to use .math functions, eg
+math.sqrt()
+math.floor()
+math.factorial() #etc!!
+round() # Rounds to the nearest integer
+abs() #always returns the positive int
+'''
+#If statements!
+#Program 1
+'''temp = input("whats the temperature?\n");
+
+if int(temp) >= 30:
+    print("It's a hot day!/n Drink Plenty of water");
+elif int(temp) <= 16:
+    print("It's a cold day'/n wear warm clothes")
+else:
+    print("It's a lovely day!!")
+'''
+
+#Program 2
+'''name = input("Enter your name: ")
+namelen = len(name)
+if namelen < 3:
+    print("Name must be more than 3 Characters")
+elif namelen > 50:
+    print("This is too much")
+else:
+    print(f"Hello! {name}")
+'''
+#program 3- The double edged weight calculator
+'''weight = float(input("Enter your weight: "))
+userChoice = input("(K)g or (L)bs: ")
+if userChoice == "k" or userChoice == "K": #You can also use userChoice.upper() or userChoice.lower
+    print(f"You are {weight * 2.205}Lbs")
+elif userChoice.upper() == "L":
+    print(f"You weigh {weight * 0.454}Kg")
+
+t = "THANK YOU"
+print(t.upper())
+'''
+
+#While Loops
+#program 4- The guessing game
+secret_number = 7
+entered = 0
+limit = 3
+while entered < limit:
+    guess = int(input("Enter your guess: "))
+    entered += 1
+    if guess == secret_number:
+        print("You guessed right!!")
+        break
+    else:
+        print("You guessed wrong")
+    
